6397ec3c89b748f55f211da5409fd52029579925
Vlad <vladov3000@gmail.com>
2016-02-01 20:57:04 -0800

    2

---
+++ src/Time.java
@@ -0,0 +1,16 @@
+package com.Economy;
+
+public class Time {
+
+    int _tm;
+    public Time(){
+
+        _tm=0;
+
+    }
+    public void tick() {
+
+        _tm ++;
+
+    }
+}
--- src/com/Economy/Main.java
+++ src/com/Economy/Main.java
@@ -2,13 +2,25 @@ package com.Economy;
 //imports
 import javafx.util.Pair;
 
+import com.Economy.Time;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
-
+/*
+def tmain():
+    while not shut:
+        time.sleep(f)
+        tm=tm+1
+ */
 public class Main {
 
+    //fields
+    static Time _tm;
+
     public static void main(String[] args) {
+
+        _tm=new Time();
+
         //commands
         Hashtable<String,Pair<Runnable, String>> commands=new Hashtable<String,Pair<Runnable, String>>();
         commands.put("/help",
@@ -20,15 +32,23 @@ public class Main {
             }
 
         }, "Shows All Commands"));
+        commands.put("/exit",
+                new Pair<Runnable, String>(
+                        () -> {
+
+                            System.out.println("Exiting... Please Standby");
+                            System.exit(0);
+                            }, "Exits Game"));
         //input
-        Scanner input = new Scanner(System.in);
-        System.out.print(">");
-        String inpt = input.nextLine();
-        if (commands.containsKey(inpt)) {
-            commands.get(inpt).getKey().run();
-        }
-        else {
-            System.out.println("ERROR[command not found]");
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            System.out.print(">");
+            String inpt = input.nextLine();
+            if (commands.containsKey(inpt)) {
+                commands.get(inpt).getKey().run();
+            } else {
+                System.out.println("ERROR[command not found]");
+            }
         }
     }
 }
