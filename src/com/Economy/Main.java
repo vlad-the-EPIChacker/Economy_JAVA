5e240a3d126fb0ba82b951af2fe642c30fc501bb
Vlad <vladov3000@gmail.com>
2016-02-24 21:10:38 -0800

    4

--- src/com/Economy/Main.java
+++ src/com/Economy/Main.java
@@ -1,18 +1,70 @@
 package com.Economy;
 //imports
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.util.Pair;
-
 import com.Economy.TimeThread;
 import javafx.util.converter.IntegerStringConverter;
-
+import javafx.application.Application;
+import javafx.stage.Stage;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
 
     //fields
     static TimeThread _tmTimeThread;
+    //make window
+
+    public void start(Stage theStage)
+    {
+        System.out.println("image = " +
+                getClass().getResource("earth.png").toString());
+        theStage.setTitle( "Timeline Example" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+        Image earth = new Image(getClass().getResource("earth.png").toString(), true);
+        Image sun = new Image(getClass().getResource("sun.png").toString(), true);
+        Image space = new Image(getClass().getResource("space.png").toString(), true);
+
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                // background image clears canvas
+                gc.drawImage( space, 0, 0 );
+                gc.drawImage( earth, x, y );
+                gc.drawImage( sun, 196, 196 );
+            }
+        }.start();
+
+        theStage.show();
+    }
 
     public interface MyCmd {
         void apply(String[] x);
@@ -22,6 +74,7 @@ public class Main {
         _tmTimeThread=new TimeThread();
         _tmTimeThread.start();
 
+        launch(new String[0]);
 
         //commands
         Hashtable<String,Pair<MyCmd, String>> commands=new Hashtable<String,Pair<MyCmd, String>>();
